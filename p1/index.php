bd72de24a71f723830ddd0259d1610ad3c837e5f
Jeremy A <azurinj@gmail.com>
2019-09-17 09:25:46 -0400

    Edited bio in HTML

--- p1/index.php
+++ p1/index.php
@@ -13,7 +13,7 @@
 
     <h2>About Me</h2>
     <p>
-      I’m a non-traditional, distance student from Washington, DC. Prior to college, I spent time off to backpack and cycle overseas, namely in South Korea and Southeast Asia but also Peru and western Europe. I returned to school in $
+      I’m a non-traditional, distance student from Washington, DC. Prior to college, I spent time off to backpack and cycle overseas, namely in South Korea and Southeast Asia but also Peru and western Europe. I returned to school in 2015 to earn my bachelor’s degree but—due to my travels—decided to study remotely through online classes. I've consequently only lived on campus for two semesters thus far and have been spending each semester traveling as a “digital nomad” to places like Saudi Arabia, South Korea (3x), Thailand, the Vatican, Cambodia, the Philippines, and Pittsburgh. I graduate this May and will be joining the Foreign Service with the US Department of State. Eventually, however, I'd like to work with animals and maybe earn a PhD in animal behavior or ecology. If I could, I'd take my dog (pictured) with me everywhere!
     </p>
   </body>
 
