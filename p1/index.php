60449a3c4ed3e58274830bf20878504b6cb3752b
Jeremy A <azurinj@gmail.com>
2019-09-17 09:42:57 -0400

    Edited bio

--- p1/index.php
+++ p1/index.php
@@ -12,12 +12,11 @@
 
     <h1>Jeremiah Azurin</h1>
 
-    <img src='images/cliff-pillow.jpg' alt='Picture of Cliff'>
+    <img src='images/cliff-pillow' alt='Picture of Cliff'>
 
     <h2>About Me</h2>
     <p>
-      I’m a non-traditional, distance student from Washington, DC. Prior to college, I took time off to backpack and cycle overseas but returned to school to earn my bachelor’s degree remotely. I consequently spend each semester traveling as a “digital nomad” and have worked in Saudi Arabia, South Korea, Thailand, the Vatican, Peru, Cambodia, the Philippines, and Pittsburgh. After graduation, I’ll be joining the Foreign Service with the US Department of State.
-    </p>
+      I’m a non-traditional, distance student from Washington, DC. Prior to college, I spent time off to backpack and cycle overseas, namely in South Korea and Southeast Asia but also Peru and western Europe. I returned to school in 2015 to earn my bachelor’s degree but—due to my travels—decided to study remotely through online classes. I've consequently only lived on campus for two semesters thus far and have been spending each semester traveling as a “digital nomad” to places like Saudi Arabia, South Korea (3x), Thailand, the Vatican, Cambodia, the Philippines, and Pittsburgh. I graduate this May and will be joining the Foreign Service with the US Department of State. Eventually, however, I'd like to work with animals and maybe earn a PhD in animal behavior or ecology. If I could, I'd take my dog (pictured) with me everywhere! </p>
 
   </body>
 
