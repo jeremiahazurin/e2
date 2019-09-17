92c6af2c87ea6a08a5d9c35acc44f72b1b873a14
Jeremy A <azurinj@gmail.com>
2019-09-17 09:24:07 -0400

    Removed container in HTML

--- p1/index.php
+++ p1/index.php
@@ -7,16 +7,14 @@
   </head>
 
   <body>
-    <div class='container'>
-      <h1>Jeremiah Azurin</h1>
+    <h1>Jeremiah Azurin</h1>
 
-      <img src='images/cliff-pillow.jpg' alt='Picture of Cliff'>
+    <img src='images/cliff-pillow.jpg' alt='Picture of Cliff'>
 
-      <h2>About Me</h2>
-      <p>
-        I’m a non-traditional, distance student from Washington, DC. Prior to college, I spent time off to backpack and cycle overseas, namely in South Korea and Southeast Asia but also Peru and western Europe. I returned to school in 2015 to earn my bachelor’s degree but—due to my travels—decided to study remotely through online classes. I've consequently only lived on campus for two semesters thus far and have been spending each semester traveling as a “digital nomad” to places like Saudi Arabia, South Korea (3x), Thailand, the Vatican, Cambodia, the Philippines, and Pittsburgh. I graduate this May and will be joining the Foreign Service with the US Department of State. Eventually, however, I'd like to work with animals and maybe earn a PhD in animal behavior or ecology. If I could, I'd take my dog (pictured) with me everywhere!
-      </p>
-    </div>
+    <h2>About Me</h2>
+    <p>
+      I’m a non-traditional, distance student from Washington, DC. Prior to college, I spent time off to backpack and cycle overseas, namely in South Korea and Southeast Asia but also Peru and western Europe. I returned to school in $
+    </p>
   </body>
 
 </html>
