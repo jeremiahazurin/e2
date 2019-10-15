bcb9e5d137ea66acbfbdcf34370f891cc53a2304
Jeremy A <azurinj@gmail.com>
2019-10-15 06:59:20 -0400

    readded process

---
+++ p3/process.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if($_GET['p1']){
+    $moves = array(Rock, Paper, Scissors);
+    $moves = rand(0,2);
+    $p1 = $_GET['p1'];
+    $p2 = $choose[$moves];
+
+if($p1 == $p2){
+    echo 'Tie';
+    } elseif($p1 == 'Rock' and $p2 == 'Scissors'){
+    echo 'Win';
+    } elseif($p1 == 'Rock' and $p2 == 'Paper'){
+    echo 'Lose';
+    }  elseif($p1 == 'Paper' and $p2 == 'Rock'){
+    echo 'Win';
+    } elseif($p1 == 'Paper' and $p2 == 'Scissors'){
+    echo 'Lose';
+    }  elseif($p1 == 'Scissors' and $p2 == 'Rock'){
+    echo 'Lose';
+    }  elseif($p1 == 'Scissors' and $p2 == 'Paper'){
+    echo 'Win';
+    }
+
+$_SESSION[' '] = $correct;
+header('Location: index.php');
