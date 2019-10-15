4ced36e3b48288ee3f9c7e34ef441ff6d96a3320
Jeremy A <azurinj@gmail.com>
2019-10-15 17:41:33 -0400

    Edited process by removing extraneous variables

--- p3/process.php
+++ p3/process.php
@@ -3,31 +3,30 @@ session_start();
 
 if($_GET["choice"]){
     $winner = null;
-    $moves = array("Rock", "Paper", "Scissors");
-    $moves = rand(0,2);
+    $moves = array("rock", "paper", "scissors");
     $p1 = $_GET["choice"];
-    $p2 =$moves[rand(0,2)];
+    $p2 = $moves[rand(0,2)];
 }
 
 if($p1 == $p2){
     echo ("Tie");
     $winner = ("Tie!");
-    } elseif($p1 == "Rock" and $p2 == "Scissors"){
+    } elseif($p1 == "rock" and $p2 == "scissors"){
     echo ("Win");
     $winner = "You won!";
-    } elseif($p1 == "Rock" and $p2 == "Paper"){
+    } elseif($p1 == "rock" and $p2 == "paper"){
     echo ("Lose");
     $winner = "You lost";
-    }  elseif($p1 == "Paper" and $p2 == "Rock"){
+    }  elseif($p1 == "paper" and $p2 == "rock"){
     echo ("Win");
     $winner = "You won!";
-    } elseif($p1 == "Paper" and $p2 == "Scissors"){
+    } elseif($p1 == "paper" and $p2 == "scissors"){
     echo ("Lose");
     $winner = "You lost";
-    }  elseif($p1 == "Scissors" and $p2 == "Rock"){
+    }  elseif($p1 == "scissors" and $p2 == "rock"){
     echo ("Lose");
     $winner = "You lost";
-    }  elseif($p1 == "Scissors" and $p2 == "Paper"){
+    }  elseif($p1 == "scissors" and $p2 == "paper"){
     echo ("Win");
     $winner = ("You won!");}
 
