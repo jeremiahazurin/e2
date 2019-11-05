5e747107b2cf4e59d12b284fa9e29dd17e3bdaaf
Jeremy A <azurinj@gmail.com>
2019-11-05 01:38:37 -0500

    Edited about page

--- zipfoods/views/about.blade.php
+++ zipfoods/views/about.blade.php
@@ -1,7 +1,1 @@
-@section('content')
-
-<h2>{{ $welcome }}</h2>
-
-<p>zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
-
-@endsection
+zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.
