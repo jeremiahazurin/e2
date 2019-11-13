8e05e0a0bfca589c47bac65fea231dc52e73af78
Jeremy A <azurinj@gmail.com>
2019-11-12 20:20:42 -0500

    add contact

--- zipfoods/views/index.blade.php
+++ zipfoods/views/index.blade.php
@@ -2,13 +2,13 @@
 
 @section('title')
 {{ $welcome }}
-@stop
+@endsection
 
 @section('content')
 
 <h2>{{ $welcome }}</h2>
 
 <p>Hello and welcome! This is the boilerplate splash page for my application built with <a
-        href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
+                href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
 
 @endsection
