13e2cdf33b4f8a586b85b37ffeb28dc65db603ac
Jeremy A <azurinj@gmail.com>
2019-11-12 23:26:19 -0500

    typo in productcontroller

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -7,7 +7,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = new Products($this->app-path('database/products.json'));
+        $products = new Products($this->app->path('database/products.json'));
         return $this->app->view('products.index', ['products' => $products]);
     }
 }
