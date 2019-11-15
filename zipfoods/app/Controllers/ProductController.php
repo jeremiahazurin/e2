4e4f47b7e52acc98fbacd24b15035f7914b5843c
Jeremy A <azurinj@gmail.com>
2019-11-14 19:18:38 -0500

    Changing productcontroller.php

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -7,10 +7,6 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = new Products($this->app->path('database/products.json'));
-        return $this->app->view(
-            'products.index',
-            ['products' => $products->getAll()]
-        );
+        return $this->app->view('products.index');
     }
 }
--- zipfoods/views/products/index.blade.php
+++ zipfoods/views/products/index.blade.php
@@ -1,16 +1,7 @@
 @extends('templates.master')
-
 @section('title')
 {{ $app->config('app.name') }}
 @endsection
-
 @section('content')
-
-
 <h2>All Products</h2>
-@foreach
-($products as $product)
-{{$product['name']}}
-@endforeach
-
 @endsection
