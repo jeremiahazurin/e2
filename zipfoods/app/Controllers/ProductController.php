e09a06da86ab518a48c0285fb2c793bbd3239013
Jeremy A <azurinj@gmail.com>
2019-11-18 22:12:18 -0500

    Added CSS

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -8,6 +8,8 @@ class ProductController extends Controller
     public function index()
     {
         $products = new Products($this->app->path('database/products.json'));
-        return $this->app->view('products.index');
+        return $this->app->view('products.index', [
+            'products' => $products ->getAll()
+            ]);
     }
 }
--- zipfoods/views/products/index.blade.php
+++ zipfoods/views/products/index.blade.php
@@ -1,7 +1,21 @@
 @extends('templates.master')
+
 @section('title')
-{{ $app->config('app.name') }}
+All Products
 @endsection
+
 @section('content')
-test
+<h2>All Products</h2>
+
+<div id='product-index'>
+    @foreach($products as $product)
+    <a href='/product?id={{ $product[' id'] }}'>
+        <div class='product'>
+            <div class='product-name'>{{ $product['name'] }}</div>
+            <img class='product-thumb' src="/images/products/{{ $product['id'] }}.jpg">
+        </div>
+    </a>
+    @endforeach
+</div>
+
 @endsection
